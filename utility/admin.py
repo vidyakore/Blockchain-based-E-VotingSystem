6cf4ac13de7c34903bf65c20e9a79c8274523a4b
ganesh-hash <62096049+ganesh-hash@users.noreply.github.com>
2022-03-27 16:28:11 +0530

    4modelsadded

--- utility/admin.py
+++ utility/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(CandidateList)
+admin.site.register(Votes)
+admin.site.register(CastedVotes)
+admin.site.register(Constituency)
--- utility/models.py
+++ utility/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.forms import IntegerField
 
 # Create your models here.
+class CandidateList(models.Model):
+     CandidateId=models.AutoField()
+     CandidateName=models.CharField(max_length=200)
+     CandidateConstituencyId=models.IntegerField()
+     CandidatePartyId=models.IntegerField()
+     CandidatePhoneNo=models.IntegerField()
+     CandidateEmail= models.EmailField(max_length=200)
+
+
+class Votes(models.Model):
+    PartyId=models.IntegerField()
+    CandidateId=models.IntegerField()
+    NoOfVotes=models.IntegerField()
+
+
+class CastedVotes(models.Model):
+    VoterId=models.IntegerField()
+    CastedVote=models.BooleanField(null=False)
+
+
+class Constituency(models.Model):
+    ConstituencyId=models.IntegerField()
+    ConstituencyName=models.CharField(max_length=200)
+    ConstituencyState=models.TextField(max_length=200)
+    ConstituencyTypeOfElection=models.TextField(max_length=200)
+    ConstituencyZipCode=models.IntegerField()
+    TotalVoters=models.IntegerField()
