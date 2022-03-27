c83653acf735f7bd03a037e8cbbe86d52e8cd386
vidyakore <vidyakore3108@gmail.com>
2022-03-27 15:51:58 +0530

    contract manager model added

--- DAppVotingSystem/utility/models.py
+++ DAppVotingSystem/utility/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class AddContractManager(models.Model):
+
+    ContractManagerId = models.IntegerField(primary_key=True)
+    ContractManagerName = models.CharField(max_length=100)
+    ContractManagerEmail = models.EmailField(max_length=50)
+    ContractManagerPhoneNo = models.CharField(max_length=12)
+    ContractManager_ConstituencyId = models.CharField(max_length=50)
+    ContractManagerPassword = models.CharField(max_length=50)
+
+
--- utility/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UtilityConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'utility'
